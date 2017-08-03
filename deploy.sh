462ab138063e758c14486daadff9f723df2a7d85
larranaga <nlarranagac@unal.edu.co>
2017-08-03 00:42:49 -0500

    fixed Oclinter and Java PMD dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,41 @@
 #!/bin/bash
-HAS_DOCKER_DAEMON = "y\n"
-MAXIMUM_CONCURRENCY = "\n" #autodetect
-HOSTNAME = "\n" #autodetect
-PORTS = "\n"  #default (64100-64200)
-MONGODB_CONFIGURATION = "\n" #default
-TASK_DIRECTORY = "." #default
-DEMONSTRATION_TASKS = "n\n"
-DOWNLOAD_DEFAULT_CONTAINER = "y\n"
-DOWNLOAD_CPP = "y\n"
-DOWNLOAD_JAVA7 = "y\n"
-DOWNLOAD_JAVA8_AND_SCALA = "n\n"
-DOWNLOAD_MONO = "n\n"
-DOWNLOAD_MOZART2 = "n\n"
-DOWNLOAD_PHP5 = "n\n"
-DOWNLOAD_PYTHIA0 = "n\n"
-DOWNLOAD_PYTHIA1 = "n\n"
-DOWNLOAD_R = "n\n"
-DOWNLOAD_SEKEXE = "n\n"
-MANUALLY_ADD_IMAGES = "y\n"
-BASE_IMAGE = "ingi/inginious-c-base"
-DEFAULT_IMAGE = "ingi/inginious-c-default"
-CPP_IMAGE = "ingi/inginious-c-cpp"
-JAVA7_IMAGE = "ingi/inginious-c-java7"
-PYTHON3_IMAGE = "ingi/inginious-c-python3"
-END_OF_IMAGES= "\n"
-MINIFIED_JS = "n\n"
-BACKUP_DIR = "."
-PLAGIARISM_DETECTOR = "n\n"
-BATCH_IMAGES = "\n"
-AUTHENTICATION = "1\n" #1 test auth 2 DB auth 3 LDAP auth
-AUTHENTICATION_METHOD_NAME = "\n" #default [DEMO]
-USERNAME_DEFAULT = "\n" #[test]
-PASSWORD_DEFAULT = "\n" #[test]
-ADITIONAL_USER = "n\n" #no aditional users
-ADITONAL_AUTH_METHOD = "n\n" #no adtional authentication methods
-SUPERADMIN = "\n" #no SUPERADMIN
-INGINIOUS_XTERM_LOCATION = "\n"
-
-
-#get docker and docker-compose
-
-sudo apt-get install -y docker docker-compose
-
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-newgrp docker
+HAS_DOCKER_DAEMON="y\n"
+MAXIMUM_CONCURRENCY="\n" #autodetect
+HOSTNAME="\n" #autodetect
+PORTS="\n"  #default (64100-64200)
+MONGODB_CONFIGURATION="\n" #default
+TASK_DIRECTORY="." #default
+DEMONSTRATION_TASKS="n\n"
+DOWNLOAD_DEFAULT_CONTAINER="y\n"
+DOWNLOAD_CPP="y\n"
+DOWNLOAD_JAVA7="y\n"
+DOWNLOAD_JAVA8_AND_SCALA="n\n"
+DOWNLOAD_MONO="n\n"
+DOWNLOAD_MOZART2="n\n"
+DOWNLOAD_PHP5="n\n"
+DOWNLOAD_PYTHIA0="n\n"
+DOWNLOAD_PYTHIA1="n\n"
+DOWNLOAD_R="n\n"
+DOWNLOAD_SEKEXE="n\n"
+MANUALLY_ADD_IMAGES="y\n"
+BASE_IMAGE="ingi/inginious-c-base"
+DEFAULT_IMAGE="ingi/inginious-c-default"
+CPP_IMAGE="ingi/inginious-c-cpp"
+JAVA7_IMAGE="ingi/inginious-c-java7"
+PYTHON3_IMAGE="ingi/inginious-c-python3"
+END_OF_IMAGES="\n"
+MINIFIED_JS="n\n"
+BACKUP_DIR="."
+PLAGIARISM_DETECTOR="n\n"
+BATCH_IMAGES="\n"
+AUTHENTICATION="1\n" #1 test auth 2 DB auth 3 LDAP auth
+AUTHENTICATION_METHOD_NAME="\n" #default [DEMO]
+USERNAME_DEFAULT="\n" #[test]
+PASSWORD_DEFAULT="\n" #[test]
+ADITIONAL_USER="n\n" #no aditional users
+ADITONAL_AUTH_METHOD="n\n" #no adtional authentication methods
+SUPERADMIN="\n" #no SUPERADMIN
+INGINIOUS_XTERM_LOCATION="\n"
 
 #clone repositories
 
@@ -54,9 +45,17 @@ git clone https://github.com/JuezUN/INGInious.git #Inginious
 git clone https://github.com/JuezUN/opt-cpp-backend.git #cpp-back-end
 git clone https://github.com/JuezUN/linter-web-service.git #linter
 
+#get docker and docker-compose
+
+apt-get install -y docker docker-compose
+
+groupadd docker
+gpasswd -a $USER docker
+newgrp docker
+
 #build containers
 
-(cd INGInious-containers ; ./build-container base && ./build-container default && ./build-container.sh cpp && ./build-container.sh java7 && ./build-container python3)
+(cd INGInious-containers && ./build-container base && ./build-container default && ./build-container.sh cpp && ./build-container.sh java7 && ./build-container python3)
 
 #install INGInious
 
