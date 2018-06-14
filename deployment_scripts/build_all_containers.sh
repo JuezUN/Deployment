3bf172880d86192f6ac68489f4bfea6964db0bd7
nicolas larranaga <nlarranagac@unal.edu.co>
2018-06-14 11:45:01 -0500

    fixed script to build containers

--- deployment_scripts/build_all_containers.sh
+++ deployment_scripts/build_all_containers.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 echo "building grading containers"
 echo "building base container"
 
-git pull --depth 1 https://github.com/JuezUN/INGInious.git
-git pull --depth 1 https://github.com/JuezUN/INGInious-containers.git
-[[ "$(docker images -q ingi/inginious-c-base 2> /dev/null)" == "" ]] && docker build -t ingi/inginious-c-base ./INGInious/base-containers/base
+path='/tmp/judge'
+
+mkdir -p $path/INGInious
+mkdir -p $path/INGInious-containers
+
+git clone --depth 1 https://github.com/JuezUN/INGInious.git $path/INGInious
+git clone --depth 1 https://github.com/JuezUN/INGInious-containers.git $path/INGInious-containers
+[[ "$(docker images -q ingi/inginious-c-base 2> /dev/null)" == "" ]] && docker build -t ingi/inginious-c-base $path/INGInious/base-containers/base
 echo "building default container"
-[[ "$(docker images -q ingi/inginious-c-default 2> /dev/null)" == "" ]] && docker build -t ingi/inginious-c-default ./INGInious/base-containers/default/
+[[ "$(docker images -q ingi/inginious-c-default 2> /dev/null)" == "" ]] && docker build -t ingi/inginious-c-default $path/INGInious/base-containers/default
 echo "building multilang container"
-[[ "$(docker images -q ingi/inginious-c-multilang 2> /dev/null)" == "" ]] && docker build -t ingi/inginious-c-multilang ./INGInious-containers/grading/multilang
+[[ "$(docker images -q ingi/inginious-c-multilang 2> /dev/null)" == "" ]] && docker build -t ingi/inginious-c-multilang $path/INGInious-containers/grading/multilang
