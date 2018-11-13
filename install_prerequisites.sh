c8fb6d980b5406cd75d1e1e4999645c2173f9cf7
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-11-13 11:18:37 -0500

    Add nginx + lighttpd to install_prerequisites script

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -8,7 +8,8 @@ bash deployment_scripts/install_basic_dependencies.sh
 sudo bash deployment_scripts/install_mongodb.sh
 bash deployment_scripts/install_node.sh
 
-echo "installing apache with mod_wsgi"
-sudo bash deployment_scripts/install_apache.sh
+echo "installing nginx + lighttpd with fastcgi"
+bash deployment_scripts/install_nginx.sh
+bash deployment_scripts/install_lighttpd.sh
 
 echo -e "\n\n$(tput setaf 3)Please logout and back in to finish the Docker installation $(tput sgr0)"
