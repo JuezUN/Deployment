f825793212eaadbce1f8540521144a402bac6fd0
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-11-29 06:33:14 -0500

    Add user ucokapi for cokapi service

--- deployment_scripts/deploy_cokapi_service.sh
+++ deployment_scripts/deploy_cokapi_service.sh
@@ -5,15 +5,22 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+id -u ucokapi > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+    useradd ucokapi
+    usermod -aG ucokapi $(whoami)
+fi
+
 docker pull unjudge/opt-cpp-backend
 docker tag unjudge/opt-cpp-backend:latest pgbovine/opt-cpp-backend:v1
 
 mkdir -p /opt/tutor
 cd /opt/tutor/
-
 git clone https://github.com/JuezUN/OnlinePythonTutor.git
-
 cd /opt/tutor/OnlinePythonTutor/v4-cokapi
+
+chown -R ucokapi:ucokapi /opt/tutor
 
 npm install express
 npm install -g jshint
