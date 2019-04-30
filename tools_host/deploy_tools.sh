14499d2acd99f8fa876383852329476d7fc0b61c
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-04-30 18:57:37 -0500

    Fix deploy tools.

--- tools_host/deploy_tools.sh
+++ tools_host/deploy_tools.sh
@@ -4,6 +4,9 @@ cd ..
 
 docker-compose up -d
 
+sudo bash tools_host/deploy_nginx_server_tools.sh
+
 sudo bash deployment_scripts/deploy_cokapi_service.sh
 
-sudo bash tools_host/deploy_nginx_server_tools.sh
+sudo systemctl restart nginx,service
+sudo sustemctl restart cokapi.service
