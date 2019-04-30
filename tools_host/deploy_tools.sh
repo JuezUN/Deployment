5019d03c407e03c46d3ee130a73d3cccb3358062
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-04-30 17:06:24 -0500

    Modify file path.

--- tools_host/deploy_tools.sh
+++ tools_host/deploy_tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sudo bash deployment_scripts/deploy_nginx_server_tools.sh
+
 cd ..
 docker-compose up -d
 sudo bash deployment_scripts/deploy_cokapi_service.sh
-sudo bash deployment_scripts/deploy_nginx_server.sh
