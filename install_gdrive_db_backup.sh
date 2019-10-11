be15c8527c21bc82dee4240d75a36b03b349a2bf
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-10-11 01:29:20 -0500

    Fix script

--- install_gdrive_db_backup.sh
+++ install_gdrive_db_backup.sh
@@ -6,7 +6,7 @@ wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
 
 mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
 chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive
+sudo install gdrive /usr/bin/gdrive
 
 echo "gdrive installed successfully"
-echo "Please run 'gdrive list' to setup Google Drive credentials"
+echo "Please run 'sudo gdrive list' to setup Google Drive credentials"
