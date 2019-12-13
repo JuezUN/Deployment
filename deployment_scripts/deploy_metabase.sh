df04304bfd5286d42afadd8b13c25faa9920d618
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-12-12 23:49:17 -0500

    Fix metabase deployment

--- deployment_scripts/deploy_metabase.sh
+++ deployment_scripts/deploy_metabase.sh
@@ -2,7 +2,7 @@
 
 echo "Setting up metabse"
 
-rm -rf ~/metabase.db
-mkdir ~/metabase.db
-cp config/metabase/*.db ~/metabase.db/
+rm -rf ~/metabase-db
+mkdir ~/metabase-db
+cp config/metabase/*.db ~/metabase-db/
 docker-compose -f config/metabase/docker-compose.yml up -d
