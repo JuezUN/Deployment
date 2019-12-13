03e929e40778744b5d59f592895b0cdf3608a82a
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-12-12 22:23:57 -0500

    Update metabase deployment with cokapi port modification

--- deployment_scripts/deploy_metabase.sh
+++ deployment_scripts/deploy_metabase.sh
@@ -2,6 +2,7 @@
 
 echo "Setting up metabse"
 
+rm -rf ~/metabase.db
 mkdir ~/metabase.db
 cp config/metabase/*.db ~/metabase.db/
 docker-compose -f config/metabase/docker-compose.yml up -d
--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ PROXY=""
 export LINTER_PORT="4567"
 export PYTHON_TUTOR_PORT="8003"
 export PYTHON_PY2_TUTOR_PORT="8004"
-export COKAPI_PORT="3000"
+export COKAPI_PORT="4000"
 export DB_PORT="27017"
 if [ -n "$PROXY" ]
 then
