86a97bc39519f978913cb9ddfa837adae92abe51
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2020-01-23 00:35:33 -0500

    Add notebook container deployment

--- deployment_scripts/build_all_containers.sh
+++ deployment_scripts/build_all_containers.sh
@@ -17,7 +17,10 @@ echo "building multilang container"
 [[ "$(docker images -q ingi/inginious-c-multilang 2> /dev/null)" == "" ]] && docker pull unjudge/inginious-c-multilang
 echo "building hdl container"
 [[ "$(docker images -q ingi/hdl-uncode 2> /dev/null)" == "" ]] && docker pull unjudge/hdl-uncode
+echo "building notebook container"
+[[ "$(docker images -q ingi/inginious-c-notebook 2> /dev/null)" == "" ]] && docker pull unjudge/inginious-c-notebook
 docker tag unjudge/inginious-c-base ingi/inginious-c-base
 docker tag unjudge/inginious-c-default ingi/inginious-c-default
 docker tag unjudge/inginious-c-multilang ingi/inginious-c-multilang
 docker tag unjudge/hdl-uncode ingi/hdl-uncode
+docker tag unjudge/inginious-c-notebook ingi/inginious-c-notebook
