c1fa2d8736880eee298fa376c1458fc2e3fd584d
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2020-06-24 22:43:16 -0500

    Fix docker image flags

--- deployment_scripts/deploy_mongo_express.sh
+++ deployment_scripts/deploy_mongo_express.sh
@@ -11,6 +11,8 @@ done
 
 printf "\nUsed username: $username\n"
 
-docker run -it --rm --network host --name uncode_mongo_express -p 8081:8081 -e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
+# Deployed in port 8081
+docker run -it --network host --name uncode_mongo_express -e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
     -e ME_CONFIG_MONGODB_SERVER="web_db_1" -e ME_CONFIG_BASICAUTH_USERNAME="$username" -e ME_CONFIG_MONGODB_PORT=27017 \
-    -e ME_CONFIG_MONGODB_SERVER=localhost -e ME_CONFIG_BASICAUTH_PASSWORD="$password" -d mongo-express
+    -e ME_CONFIG_MONGODB_SERVER=localhost -e ME_CONFIG_BASICAUTH_PASSWORD="$password" \
+    -e ME_CONFIG_SITE_BASEURL="/mongo_express" -d --restart always mongo-express
