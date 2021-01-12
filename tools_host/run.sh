5536df46e7674e60c0a52b5c3367abaaa8e4870e
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2021-01-11 23:46:58 -0500

    Fix tools host

--- tools_host/run.sh
+++ tools_host/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo $DEPLOYMENT_HOME/tools_host/deploy_tools.sh
+docker-compose --compatibility -f $DEPLOYMENT_HOME/docker-compose.yml up -d
+
+sudo bash $DEPLOYMENT_HOME/tools_host/deploy_cokapi_service.sh
+sudo systemctl restart cokapi.service
 
 sudo $DEPLOYMENT_HOME/tools_host/deploy_nginx_server_tools.sh
 
