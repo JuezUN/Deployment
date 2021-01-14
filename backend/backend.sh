7fce7d85bc5af4c76d3cdd06797df51682dc67cd
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2021-01-13 20:26:17 -0500

    Some fixes on backend

--- backend/backend.sh
+++ backend/backend.sh
@@ -3,4 +3,4 @@
 # Starts the docker-agent module from inginious
 set -e
 
-inginious-backend tcp://127.0.0.1:2001 tcp://127.0.0.1:2000
+inginious-backend tcp://$UNCODE_DOMAIN:2001 tcp://127.0.0.1:2000
