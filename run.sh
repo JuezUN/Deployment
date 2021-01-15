25694d01c90c9a8c6243fefb016ce0a953667968
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2021-01-14 23:09:13 -0500

    Fix uncode code paths

--- run.sh
+++ run.sh
@@ -13,8 +13,8 @@ sudo $DEPLOYMENT_HOME/deployment_scripts/build_all_containers.sh
 
 sudo $DEPLOYMENT_HOME/backend/install_backend_service.sh
 
-sudo bash $DEPLOYMENT_HOME/agent/deploy_agent_services.sh
-sudo systemctl start docker_agent && sudo systemctl start mcq_agent
-
 sudo bash $DEPLOYMENT_HOME/deployment_scripts/deploy_nginx_server.sh
 sudo bash $DEPLOYMENT_HOME/deployment_scripts/deploy_lighttpd_server.sh
+
+sudo bash $DEPLOYMENT_HOME/agent/deploy_agent_services.sh
+sudo systemctl start docker_agent && sudo systemctl start mcq_agent
